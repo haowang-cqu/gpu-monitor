19ff728f2aed13cab87f55cbf13577df4b216bfe
Hao Wang <iamwh.cn@gmail.com>
2022-07-22 20:09:37 +0800

    初步完成状态监控

--- gm.py
+++ gm.py
@@ -79,6 +79,33 @@ def nvidia_smi():
 def process():
     return jsonify(all_gpu_process())
 
+@app.route("/status")
+def status():
+    nvidia_smi = subprocess.check_output(['nvidia-smi', '-q', '-x'])
+    root = ET.fromstring(nvidia_smi)
+    status_list = []
+    for gpu_id, gpu in enumerate(root.findall("gpu")):
+        status_list.append({
+            "gpu_id": gpu_id,
+            "product_name": gpu.find("product_name").text,
+            "fan_speed": int(gpu.find("fan_speed").text.split()[0]),
+            "memory_total": int(gpu.find("fb_memory_usage/total").text.split()[0]),
+            "memory_used": int(gpu.find("fb_memory_usage/used").text.split()[0]),
+            "utilization": int(gpu.find("utilization/gpu_util").text.split()[0]),
+            "temperature": int(gpu.find("temperature/gpu_temp").text.split()[0]),
+            "power_limit": float(gpu.find("power_readings/power_limit").text.split()[0]),
+            "power_draw": float(gpu.find("power_readings/power_draw").text.split()[0]),
+            "power_state": gpu.find("power_readings/power_state").text,
+            "process_number": len(gpu.findall(".//process_info")),
+        })
+    data = {
+        "timestamp": root.find("timestamp").text,
+        "driver_version": root.find("driver_version").text,
+        "cuda_version": root.find("cuda_version").text,
+        "attached_gpus": int(root.find("attached_gpus").text),
+        "status": status_list
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=33090)
+    app.run(host="0.0.0.0", port=12345)
